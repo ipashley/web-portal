3f6cceb91d18f65a84b51456db53945072df3b1e
unknown <mbeperajackline@gmail.com>
2024-08-22 14:00:27 +0300

    last changes

--- resources/views/pages/aboutus.blade.php
+++ resources/views/pages/aboutus.blade.php
@@ -20,7 +20,17 @@
             <h2 id="about"></h2><b>About Us</b></h2>
             <p id="content">HubSpot’s company and culture are a lot like our product. They’re crafted, not cobbled, for a delightful experience.</p>
             </div>
-      
+     </div>
+
+     <div class="container1" style="margin-top: 10px;">
+        <div class="right1">
+            <h2 id="about"></h2><b>About Us</b></h2>
+            <p id="content">HubSpot’s company and culture are a lot like our product. They’re crafted, not cobbled, for a delightful experience.</p>
+            </div>
+        <div class="left1"> 
+           
+            <img src="image/image2.jpeg"></img>
+        </div>
      </div>
     
     
